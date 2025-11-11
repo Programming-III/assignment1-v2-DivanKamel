7461161cbb89b78360fc1dceee7b204b2fba88c0
DivanKamel <divanmourad@gmail.com>
2025-11-11 09:38:44 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,10 +5,41 @@
 #include "Instructor.h"
 #include "Course.h"
 
-
+#include <iostream>
 using namespace std;
 
 // ==================== Person Class Implementation =========================
+class Person(){
+   public:
+      Person(string name, int id){
+        this -> name = name;
+        this -> id = id;
+      }
+      ~Person(){        
+      }
+      Person(string name){
+        this -> name = name;
+        id = 0;
+      }
+
+    string getName(){
+        return this -> name;
+    }  
+    int getId(){
+        return this ->id;
+    }
+    void setName(string name){
+        this -> name = name;
+    }
+    void setId(int id){
+        this -> id= id;
+    }
+
+    string display(){
+        return name << ": "<< id;
+    }
+}
+
 
 
 
