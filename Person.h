dba808948856e0a791ea2e030ca1b750bc55115b
DivanKamel <divanmourad@gmail.com>
2025-11-11 09:33:42 +0200

    Update Person.h

--- Person.h
+++ Person.h
@@ -3,6 +3,40 @@
 
 #include <string>
 using namespace std;
+class Person(){
+  private:
+      string name;
+      int id;
+  public:
+      Person(string name, int id){
+        this -> name = name;
+        this -> id = id;
+      }
+      ~Person(){        
+      }
+      Person(string name){
+        this -> name = name;
+        id = 0;
+      }
+
+    string getName(){
+        return this -> name;
+    }  
+    int getId(){
+        return this ->id;
+    }
+    void setName(string name){
+        this -> name = name;
+    }
+    void setId(int id){
+        this -> id= id;
+    }
+
+    string display(){
+        return name << ": "<< id;
+    }
+}
+
 
 //#write Person class here
 
