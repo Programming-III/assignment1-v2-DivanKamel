7227c767a16fde3f4d1f6eb2bacb759d2d7d9915
DivanKamel <divanmourad@gmail.com>
2025-11-11 09:35:42 +0200

    Update Instructor.h

--- Instructor.h
+++ Instructor.h
@@ -3,7 +3,38 @@
 
 #include "Person.h"
 using namespace std;
-//#write Instructor class here
+class Instructor : public Person(){
+    private:
+        string dep;
+        int expLev;
+    public:
+        Instructor(string d, int ex){
+            dep = d;
+            expLev = ex;
+        }
+        Instructor(string d){
+            dep = d;
+            expLev=0;
+        }
+        ~Instructor(){            
+        }
+        string getDepartment(){
+            return dep;
+        }
+        int getExperienceLevel(){
+            return expLev;
+        }
+        void setDepartment(string d){
+            dep = d;
+        }
+        void setExperienceLevel(int e){
+            expLev = e;
+        }
+
+        string display(){
+            return "Name: "<<Instructor.getName<<endl<<"Department: " << dep << endl << "Experience: " << expLev;
+        }
+}
 
 
 
