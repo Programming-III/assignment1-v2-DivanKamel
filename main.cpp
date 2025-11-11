81125d951c1be3f0427c9f6a6ca26af89e1dfcda
DivanKamel <divanmourad@gmail.com>
2025-11-11 09:41:11 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -85,6 +85,35 @@ public:
 
 
 // ==================== Instructor Class Implementation ====================
+class Instructor : public Person(){
+    public:
+        Instructor(string d, int ex){
+            dep = d;
+            expLev = ex;
+        }
+        Instructor(string d){
+            dep = d;
+            expLev=0;
+        }
+        ~Instructor(){            
+        }
+        string getDepartment(){
+            return dep;
+        }
+        int getExperienceLevel(){
+            return expLev;
+        }
+        void setDepartment(string d){
+            dep = d;
+        }
+        void setExperienceLevel(int e){
+            expLev = e;
+        }
+
+        string display(){
+            return "Name: "<<Instructor.getName<<endl<<"Department: " << dep << endl << "Experience: " << expLev;
+        }
+}
 
 
 
