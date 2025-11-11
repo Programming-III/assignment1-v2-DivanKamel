bbfe19e33c94267292180a3eb03d618c03026e76
DivanKamel <divanmourad@gmail.com>
2025-11-11 09:41:58 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -121,6 +121,33 @@ class Instructor : public Person(){
 
 
 // ==================== Course Class Implementation ====================
+class Course(){
+    
+     public:
+        Course(string cc , string n, int m, Student* s, int c){
+            courseCode= cc;
+            name = n;
+            max = m;
+            s = *Student[50]; 
+            current=c; 
+        }   
+        Course(name, max , current){
+            this -> name = name;
+            this -> max = max;
+            this -> current = current;
+        }
+        ~Course(){
+            delete students[];
+        }
+
+        void addStudent(const Student& s){
+            students.add(s);
+        }
+        string displayCourseInfo(){
+            return "Course: "<< courseCode << "- "<< name << "Max Students: "<< max << "Currently Enrolled" << current; 
+        }
+ }
+
 
 
 
