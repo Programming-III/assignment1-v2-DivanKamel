d9a0c80bfb3e9004f51ae726a3a1b36b4c787d43
DivanKamel <divanmourad@gmail.com>
2025-11-11 09:36:24 +0200

    Update Course.h

--- Course.h
+++ Course.h
@@ -4,7 +4,38 @@
 #include <string>
 #include "Student.h"
 using namespace std;
-//#write your code here
+class Course(){
+    private:
+        string courseCode;
+        string name;
+        int max;
+        Student* students; 
+        int current;
+
+     public:
+        Course(string cc , string n, int m, Student* s, int c){
+            courseCode= cc;
+            name = n;
+            max = m;
+            s = *Student[50]; 
+            current=c; 
+        }   
+        Course(name, max , current){
+            this -> name = name;
+            this -> max = max;
+            this -> current = current;
+        }
+        ~Course(){
+            delete students[];
+        }
+
+        void addStudent(const Student& s){
+            students.add(s);
+        }
+        string displayCourseInfo(){
+            return "Course: "<< courseCode << "- "<< name << "Max Students: "<< max << "Currently Enrolled" << current; 
+        }
+ }
 
 
 
